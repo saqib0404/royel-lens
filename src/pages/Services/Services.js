174bea57b28ad74499e764843e672042c6ee59e3
Saqib Ahmad <you@example.com>
2023-03-12 17:45:28 +0600

    services andabout page made

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const Services = () => {
     return (
         <div className='h-screen'>
-            <h3>Page Under Construction</h3>
-            <button>Go Back</button>
+            <h3 className='text-3xl font-semibold text-center mt-16'>Page Under Construction</h3>
+            <Link to='/' className='btn w-max btn-accent text-center flex items-center mx-auto mt-5'>Go Back &nbsp;<AiOutlineArrowLeft /></Link>
         </div>
     );
 };
