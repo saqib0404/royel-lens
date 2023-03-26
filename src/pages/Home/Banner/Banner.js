5fdac197d3186ae5794476522db199c78609a96c
Saqib Ahmad <you@example.com>
2023-03-26 10:09:05 +0600

    foter modified

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import banner from '../../../assets/banner.jpg';
+import './Banner.css';
 
 const Banner = () => {
     return (
         <div>
-            
+            <img className='banner' src={banner} alt="" />
         </div>
     );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import './Home.css';
+import { BsMessenger } from "react-icons/bs";
 
 const Home = () => {
     return (
-        <>
+        <div>
+            <BsMessenger className="msnger" size={40} />
             <Banner />
-        </>
+        </div>
     );
 };
 
